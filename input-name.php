a0a904fb1c794f7666ba241a3526dccf61ea9184
flukekazo55 <44312069+flukekazo55@users.noreply.github.com>
2019-03-15 00:07:06 +0700

    first

---
+++ input-name.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>FUNPHABET</title>
+  <link rel="icon" type="image/gif/png" href="img/castle.png" alt="https://www.flaticon.com">
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+  <!-- Plugin CSS -->
+  <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
+
+  <!-- Custom styles for this template -->
+  <link href="css/freelancer.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg bg-secondary fixed-top text-uppercase" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">FUNPHABET</a>
+      <button class="navbar-toggler navbar-toggler-right text-uppercase bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+		  <li class="nav-item mx-0 mx-lg-1">
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger portfolio-item" href="#portfolio-modal-1">คำอธิบาย</a>
+          </li>
+		  <li class="nav-item mx-0 mx-lg-1">
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger portfolio-item" href="#portfolio-modal-2">ดูคะแนน</a>
+          </li>
+          <li class="nav-item mx-0 mx-lg-1">
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger portfolio-item" href="#portfolio-modal-3">ติดต่อ</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Header -->
+  <header class="masthead text-center">
+    <div class="container">
+		<img class="img-fluid mb-5 d-block mx-auto img-logo" src="img/castle.png" alt="https://www.flaticon.com">
+	    <h1>FUNPHABET</h1>
+	    <hr class="star-dark">
+		<div align="middle">
+			<form action="input-name.php" method="post">
+				<div class="row">
+					<div class="col-md-4"></div>
+					<div class="col-md-4" align="middle">
+						<div class="form-group">
+							<input type="text" class="input" placeholder="กรุณาใส่ชื่อ" name="name" required>
+						</div>
+						<button type="submit" class="btn2" id="sendMessageButton">ยืนยัน</button>
+					</div>
+					<div class="col-md-4"></div>
+				</div>
+			</form>
+		<div>
+		</div>
+		<hr class="star-dark">
+    </div>
+  </header>
+
+  <div class="copyright py-4 text-center text-white">
+    <div class="container">
+      <small>Copyright &copy; ART And Creative 2019</small>
+    </div>
+  </div>
+
+  <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+  <div class="scroll-to-top d-lg-none position-fixed ">
+    <a class="js-scroll-trigger d-block text-center text-white rounded" href="#page-top">
+      <i class="fa fa-chevron-up"></i>
+    </a>
+  </div>
+
+  <!-- Portfolio Modals -->
+
+  <!-- Portfolio Modal 1 -->
+  <div class="portfolio-modal mfp-hide" id="portfolio-modal-1">
+    <div class="portfolio-modal-dialog bg-white">
+      <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
+        <i class="fa fa-3x fa-times"></i>
+      </a>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-lg-6 mx-auto">
+            <h2 class="text-secondary text-uppercase mb-0">คำอธิบาย</h2>
+            <hr class="star-dark mb-5">
+            <img class="img-fluid mb-5 img-port" src="img/discription.png" alt="https://www.flaticon.com">
+            <p class="mb-5">ข้อความ</p>
+            <a class="btn btn-danger btn-lg rounded-pill portfolio-modal-dismiss" href="#">
+              <i class="fa fa-close"></i>
+			ปิด</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <div class="portfolio-modal mfp-hide" id="portfolio-modal-2">
+    <div class="portfolio-modal-dialog bg-white">
+      <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
+        <i class="fa fa-3x fa-times"></i>
+      </a>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-lg-6 mx-auto">
+            <h2 class="text-secondary text-uppercase mb-0">ดูคะแนน</h2>
+            <hr class="star-dark mb-5">
+            <img class="img-fluid mb-5 img-port" src="img/rank.png" alt="https://www.flaticon.com">
+            <p class="mb-5">ข้อความ</p>
+            <a class="btn btn-danger btn-lg rounded-pill portfolio-modal-dismiss" href="#">
+              <i class="fa fa-close"></i>
+			ปิด</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <div class="portfolio-modal mfp-hide" id="portfolio-modal-3">
+    <div class="portfolio-modal-dialog bg-white">
+      <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
+        <i class="fa fa-3x fa-times"></i>
+      </a>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-lg-6 mx-auto">
+            <h2 class="text-secondary text-uppercase mb-0">ติดต่อ</h2>
+			<hr class="star-dark mb-5">
+            <img class="img-fluid mb-5 img-port" src="img/contract.png" alt="https://www.flaticon.com">
+			<div class="col-lg-12" align="middle">
+				<a href="#" class="btn-facebook">
+					<i class="fab fa-facebook-f"></i>
+				</a>
+				<a href="#" class="btn-google">
+					<i class="fab fa-google"></i>
+				</a>
+				<a href="#" class="btn-instagram">
+					<i class="fab fa-instagram"></i>
+				</a>
+				<a href="#" class="btn-line">
+					<i class="fab fa-line"></i>
+				</a>
+			</div>
+			<hr class="star-dark mb-5">
+            <a class="btn btn-danger btn-lg rounded-pill portfolio-modal-dismiss" href="#">
+              <i class="fa fa-close"></i>
+			ปิด</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+  <!-- Contact Form JavaScript -->
+  <script src="js/jqBootstrapValidation.js"></script>
+  <script src="js/contact_me.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/freelancer.js"></script>
+</body>
+
+</html>
